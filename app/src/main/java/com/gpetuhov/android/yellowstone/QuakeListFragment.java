c8c9f25793fc33924a444a1dbaa06107496810e1
Gennadiy Petuhov <g3pmail@gmail.com>
2016-09-09 17:53:22 +0400

    Add activity with one empty fragment

---
+++ app/src/main/java/com/gpetuhov/android/yellowstone/QuakeListFragment.java
@@ -0,0 +1,23 @@
+package com.gpetuhov.android.yellowstone;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+// Fragment contains list of earthquakes
+public class QuakeListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_quakelist, container, false);
+
+        return v;
+    }
+
+}
---
+++ app/src/main/java/com/gpetuhov/android/yellowstone/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.gpetuhov.android.yellowstone;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+// Abstract class for activity with one fragment
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    // Abstract method for creating fragment (override with concrete fragment)
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        // Add fragment to activity
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
+
+}
