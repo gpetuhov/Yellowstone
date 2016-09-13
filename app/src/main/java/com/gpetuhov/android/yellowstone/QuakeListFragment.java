378b721452bd8d44d3d0ae6a67e890d68034e76e
Gennadiy Petuhov <g3pmail@gmail.com>
2016-09-13 13:53:07 +0400

    Add RecyclerView with placeholder data

--- app/src/main/java/com/gpetuhov/android/yellowstone/QuakeListFragment.java
+++ app/src/main/java/com/gpetuhov/android/yellowstone/QuakeListFragment.java
@@ -3,21 +3,102 @@ package com.gpetuhov.android.yellowstone;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // Fragment contains list of earthquakes
 public class QuakeListFragment extends Fragment {
+
+    private RecyclerView mQuakeRecyclerView;    // RecyclerView for list of quakes
+    private QuakeAdapter mAdapter;              // Adapter for RecyclerView
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_quakelist, container, false);
+        View v = inflater.inflate(R.layout.fragment_quake_list, container, false);
+
+        // Get access to RecyclerView
+        mQuakeRecyclerView = (RecyclerView) v.findViewById(R.id.quake_recycler_view);
+
+        // Set LinearLayoutManager for our RecyclerView (we need vertical scroll list)
+        mQuakeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUI();
 
         return v;
+    }
+
+    // Set adapter for our RecyclerView
+    private void updateUI() {
+        mAdapter = new QuakeAdapter();
+        mQuakeRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    // === Inner classes =====================
+
+    // ViewHolder for our RecyclerView with list of earthquakes
+    private class QuakeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView; // Text for one list row
+
+        public QuakeHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView; // In our test code itemView is one line of text. This will change later
+        }
+    }
+
+
+    // Adapter for our RecyclerView with list of earthquakes
+    private class QuakeAdapter extends RecyclerView.Adapter<QuakeHolder> {
+
+        private List<String> mQuakes;   // Stores list of earthquakes
+
+        // In constructor we create simple list of strings for test purpose
+        public QuakeAdapter() {
+            mQuakes = new ArrayList<>();
+
+            for (int i = 0; i < 100; i++) {
+                mQuakes.add("Quake " + i);
+            }
+        }
+
+        @Override
+        public QuakeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // Get LayoutInflater from parent activity
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            // Create view for one list item from item layout
+            View view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false);
+
+            // Create ViewHolder with inflated view for one list item
+            return new QuakeHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(QuakeHolder holder, int position) {
+            // Get earthquake at "position" from list of quakes
+            String quake = mQuakes.get(position);
+
+            // Set ViewHolder of list item according to earthquake at "position"
+            holder.mTitleTextView.setText(quake);
+        }
+
+        @Override
+        public int getItemCount() {
+            // Return size of list of earthquakes
+            return mQuakes.size();
+        }
     }
 
 }
